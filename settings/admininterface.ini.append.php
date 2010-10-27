f21ba8fcb283f35ab36e6e92e89a65690cf5cfd6
Christian Pfeffer Gjengedal <cpg@ez.no>
2010-10-27 16:36:49 +0200

    Fixed bug #017556: CJW Newsletter misses admininterface.ini.append.php

---
+++ settings/admininterface.ini.append.php
@@ -0,0 +1,7 @@
+<?php /* #?ini charset="utf-8"?
+
+[SubItems]
+# Column names reflect the attribute names of template variable $node
+VisibleColumns[eznewsletternavigationpart]=checkbox;crank;name;creator;published_date;translations
+
+*/ ?>
