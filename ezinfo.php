2a2a89305bc43c32349b07f1e1b7a08f74c5ed95
felix <felix@felix-hst>
2011-02-11 15:29:11 +0100

    version: 1.0.0.201102111523

--- ezinfo.php
+++ ezinfo.php
@@ -16,7 +16,7 @@
 class cjw_newsletterInfo
 {
     // set manually - is used in email header, and in file header @version
-    const SOFTWARE_VERSION = '1.0.0.201102101723';
+    const SOFTWARE_VERSION = '1.0.0.201102111523';
 
     static function info()
     {
